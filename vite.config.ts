47818ee05ac91b393166509b58cf5280a17f5383
Tom Nunan <greywizardtea@gmail.com>
2024-11-29 00:59:49 +1100

    Fix build configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/Character-Planner/",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: true,
+  },
 });
